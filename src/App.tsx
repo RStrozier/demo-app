131a46464a6afaea3370ba01f53bfd5df5a8d3d2
Strozier <strozierrashida@vineyardcolumbus.org>
2024-06-28 09:55:48 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className="div">
+      <p>Testing this to see how good I can do at connection</p>
+     </div>
+       
+    </>
+  )
+}
+
+export default App
